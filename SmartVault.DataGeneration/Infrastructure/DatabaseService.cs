43be0c64189a19a2b94fb5605cf716c7c8abc7b1
JoaquindelaCanal <joaquindelacanal@hotmail.com>
2024-08-28 17:09:07 -0300

    Moving the database logic to a separate class, the code is more modular and easier to maintain or extend

---
+++ SmartVault.DataGeneration/Infrastructure/DatabaseService.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+
+using Microsoft.Extensions.Configuration;
+
+using SmartVault.Library;
+
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SmartVault.DataGeneration.Infrastructure
+{
+    public class DatabaseService : IDisposable
+    {
+        private readonly SQLiteConnection _connection;
+
+        public DatabaseService(string connectionString)
+        {
+            _connection = new SQLiteConnection(connectionString);
+            _connection.Open();
+        }
+
+        public void InitializeDatabase(string databaseFileName)
+        {
+            SQLiteConnection.CreateFile(databaseFileName);
+        }
+
+        public void ExecuteScriptFromFile(string filePath)
+        {
+            var serializer = new XmlSerializer(typeof(BusinessObject));
+            using var reader = new StreamReader(filePath);
+            var businessObject = serializer.Deserialize(reader) as BusinessObject;
+
+            if (businessObject != null)
+            {
+                _connection.Execute(businessObject.Script);
+            }
+        }
+
+        public void InsertUser(int id, string firstName, string lastName, DateTime dateOfBirth, int accountId, string username, string password)
+        {
+            var sql = "INSERT INTO User (Id, FirstName, LastName, DateOfBirth, AccountId, Username, Password) VALUES (@Id, @FirstName, @LastName, @DateOfBirth, @AccountId, @Username, @Password)";
+            _connection.Execute(sql, new { Id = id, FirstName = firstName, LastName = lastName, DateOfBirth = dateOfBirth, AccountId = accountId, Username = username, Password = password });
+        }
+
+        public void InsertAccount(int id, string name)
+        {
+            var sql = "INSERT INTO Account (Id, Name) VALUES (@Id, @Name)";
+            _connection.Execute(sql, new { Id = id, Name = name });
+        }
+
+        public void InsertDocument(int id, string name, string filePath, long length, int accountId)
+        {
+            var sql = "INSERT INTO Document (Id, Name, FilePath, Length, AccountId) VALUES (@Id, @Name, @FilePath, @Length, @AccountId)";
+            _connection.Execute(sql, new { Id = id, Name = name, FilePath = filePath, Length = length, AccountId = accountId });
+        }
+
+        public int GetCount(string tableName)
+        {
+            return _connection.ExecuteScalar<int>($"SELECT COUNT(*) FROM {tableName};");
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
--- SmartVault.DataGeneration/Program.cs
+++ SmartVault.DataGeneration/Program.cs
@@ -1,12 +1,13 @@
-﻿using Dapper;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+
 using Newtonsoft.Json;
-using SmartVault.Library;
+
+using SmartVault.DataGeneration.Infrastructure;
+
 using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
 using System.IO;
-using System.Xml.Serialization;
+using System.Linq;
 
 namespace SmartVault.DataGeneration
 {
@@ -18,50 +19,52 @@ namespace SmartVault.DataGeneration
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json").Build();
 
-            SQLiteConnection.CreateFile(configuration["DatabaseFileName"]);
-            File.WriteAllText("TestDoc.txt", $"This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}");
+            string databaseFileName = configuration["DatabaseFileName"];
+            string connectionString = string.Format(configuration["ConnectionStrings:DefaultConnection"], databaseFileName);
 
-            using (var connection = new SQLiteConnection(string.Format(configuration?["ConnectionStrings:DefaultConnection"] ?? "", configuration?["DatabaseFileName"])))
+            var documentPath = Path.Combine(Directory.GetCurrentDirectory(), "TestDoc.txt");
+            File.WriteAllText(documentPath, GenerateTestDocument());
+
+            using (var dbService = new DatabaseService(connectionString))
             {
+                dbService.InitializeDatabase(databaseFileName);
+
                 var files = Directory.GetFiles(@"..\..\..\..\BusinessObjectSchema");
                 for (int i = 0; i <= 2; i++)
                 {
-                    var serializer = new XmlSerializer(typeof(BusinessObject));
-                    var businessObject = serializer.Deserialize(new StreamReader(files[i])) as BusinessObject;
-                    connection.Execute(businessObject?.Script);
-
+                    dbService.ExecuteScriptFromFile(files[i]);
                 }
+
                 var documentNumber = 0;
                 for (int i = 0; i < 100; i++)
                 {
-                    var randomDayIterator = RandomDay().GetEnumerator();
-                    randomDayIterator.MoveNext();
-                    connection.Execute($"INSERT INTO User (Id, FirstName, LastName, DateOfBirth, AccountId, Username, Password) VALUES('{i}','FName{i}','LName{i}','{randomDayIterator.Current.ToString("yyyy-MM-dd")}','{i}','UserName-{i}','e10adc3949ba59abbe56e057f20f883e')");
-                    connection.Execute($"INSERT INTO Account (Id, Name) VALUES('{i}','Account{i}')");
+                    var randomDay = RandomDay();
+                    dbService.InsertUser(i, $"FName{i}", $"LName{i}", randomDay, i, $"UserName-{i}", "e10adc3949ba59abbe56e057f20f883e");
+                    dbService.InsertAccount(i, $"Account{i}");
 
                     for (int d = 0; d < 10000; d++, documentNumber++)
                     {
-                        var documentPath = new FileInfo("TestDoc.txt").FullName;
-                        connection.Execute($"INSERT INTO Document (Id, Name, FilePath, Length, AccountId) VALUES('{documentNumber}','Document{i}-{d}.txt','{documentPath}','{new FileInfo(documentPath).Length}','{i}')");
+                        dbService.InsertDocument(documentNumber, $"Document{i}-{d}.txt", documentPath, new FileInfo(documentPath).Length, i);
                     }
                 }
 
-                var accountData = connection.Query("SELECT COUNT(*) FROM Account;");
-                Console.WriteLine($"AccountCount: {JsonConvert.SerializeObject(accountData)}");
-                var documentData = connection.Query("SELECT COUNT(*) FROM Document;");
-                Console.WriteLine($"DocumentCount: {JsonConvert.SerializeObject(documentData)}");
-                var userData = connection.Query("SELECT COUNT(*) FROM User;");
-                Console.WriteLine($"UserCount: {JsonConvert.SerializeObject(userData)}");
+                Console.WriteLine($"AccountCount: {dbService.GetCount("Account")}");
+                Console.WriteLine($"DocumentCount: {dbService.GetCount("Document")}");
+                Console.WriteLine($"UserCount: {dbService.GetCount("User")}");
             }
         }
 
-        static IEnumerable<DateTime> RandomDay()
+        static string GenerateTestDocument()
+        {
+            return string.Join(Environment.NewLine, new string[100].Select(_ => "This is my test document"));
+        }
+
+        static DateTime RandomDay()
         {
             DateTime start = new DateTime(1985, 1, 1);
             Random gen = new Random();
             int range = (DateTime.Today - start).Days;
-            while (true)
-                yield return start.AddDays(gen.Next(range));
+            return start.AddDays(gen.Next(range));
         }
     }
 }
